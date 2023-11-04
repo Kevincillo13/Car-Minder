b48c5513c805437cd12e4d978102a0ed40fa8781
Kevincillo13 <crkevin105@gmail.com>
2023-11-03 23:51:33 -0600

    Ya funciona el register, yujuuu

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const db = mysql.createConnection({
     port: "3308",
     user: "root",
     password: "",
-    database: "car_minder"
+    database: "carminder"
 });
 
 db.connect((err)=>{
@@ -39,16 +39,32 @@ app.get("/Car-Minder",(req,res)=>{
 })
 
     //Register
-app.post("/register", (req, res)=>{
-    const{nombre_u, correo_u, contraseña_u} = req.body;
-    const query= "INSERT INTO usuario (nombre_u, contraseña_u, correo_u) VALUES (?, ?, ?)";
-    db.query(query, [nombre_u, contraseña_u, correo_u], (err, result)=>{
-        if (err){
-            console.error("Error al registrar usuario: ", err);
-            res.send("Error al registrar el usuario");
-        } else {
-            console.log("Usuario registrado con éxito");
-            res.send("Usuario registrado con éxito");
-        }
-    });
-});
+    app.post("/register", (req, res) => {
+        const { nombre_u, correo_u, contraseña_u } = req.body;
+      
+        // Verificar si el correo electrónico ya existe en la base de datos
+        const queryCheck = "SELECT * FROM usuarios WHERE correo_u = ?";
+        db.query(queryCheck, [correo_u], (err, results) => {
+          if (err) {
+            console.error("Error al verificar el correo electrónico: ", err);
+            res.status(500).send('<script>alert("Error al verificar el correo electrónico"); window.location="/Car-Minder";</script>');
+          } else if (results.length > 0) {
+            // El correo electrónico ya está en uso
+            res.status(400).send('<script>alert("El correo electrónico ya está en uso. Por favor, elija otro."); window.location="/Car-Minder";</script>');
+          } else {
+            // El correo electrónico no está en uso, procede con la inserción
+            const query = "INSERT INTO usuarios (nombre_u, contraseña_u, correo_u, created_at, active) VALUES (?, ?, ?, NOW(), 1)";
+            db.query(query, [nombre_u, contraseña_u, correo_u], (err, result) => {
+              if (err) {
+                console.error("Error al registrar usuario: ", err);
+                res.status(500).send('<script>alert("Error al registrar al usuario"); window.location="/Car-Minder";</script>');
+              } else {
+                console.log("Usuario registrado con éxito");
+      
+                // Muestra una alerta en el navegador del cliente
+                res.send('<script>alert("Usuario registrado con éxito"); window.location="/Car-Minder";</script>');
+              }
+            });
+          }
+        });
+      });
