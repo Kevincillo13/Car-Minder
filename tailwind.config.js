88d73ce818a35dbea10f766509f7e622fdedee14
Zaredzin <JammedMist76@hotmail.com>
2023-11-07 22:01:47 -0600

    Arreglé login-register

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,28 @@
+const colors = require('tailwindcss/colors')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [    './views/**/*.ejs',],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "Gabarito": ['Gabarito','sans-serif']
+      },
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        black: colors.black,
+        white: colors.white,
+        emerald: colors.emerald,
+        indigo: colors.indigo,
+        yellow: colors.yellow,
+        stone: colors.warmGray,
+        sky: colors.lightBlue,
+        neutral: colors.trueGray,
+        gray: colors.coolGray,
+        slate: colors.blueGray,
+
+  }
+    },
   },
   plugins: [],
 }
